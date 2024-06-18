3a330a52a37b34bc6674093a0d86c7de98550d60
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-06-18 23:09:00 +0530

    react router dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1 className="text-3xl font-bold underline">Home Page</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "search/:keywords",
+    element: (
+      <div>
+        <h1 className="text-3xl font-bold underline">Search Results</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "bookmarks",
+    element: (
+      <div>
+        <h1 className="text-3xl font-bold underline">Bookmarks</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: (
+      <div>
+        <h1 className="text-3xl font-bold underline">About Us</h1>
+      </div>
+    ),
+  },
+  {
+    path: "*",
+    element: (
+      <div>
+        <h1 className="text-3xl font-bold underline">404</h1>
+        <h2 className="text-2xl font-bold underline">Page Not Found</h2>
+      </div>
+    ),
+  },
+]);
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <RouterProvider router={router} />;
 }
